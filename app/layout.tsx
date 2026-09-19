82df918e85725018bd4f081ba5fb2caa37b832e4
Krishna Avtar <krishnaavtar955@gmail.com>
2026-09-19 16:08:49 +0530

    feat: add root layout component with metadata and providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppShell from "./components/AppShell";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
 import DefaultCanonical from "./components/DefaultCanonical";
+
+const GTM_ID = "GTM-WP77FSW2";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,6 +49,27 @@ export default function RootLayout({
         className="min-h-full bg-slate-50 dark:bg-slate-800 flex flex-col"
         suppressHydrationWarning
       >
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+        {/* End Google Tag Manager */}
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <DefaultCanonical />
         <AuthProvider>
           <AppShell>
